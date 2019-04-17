4f34e8acb7cc6d29e1865354b3a1ccfe12aa36a5
D00215593 <d00215593@student.dkit.ie>
2019-04-17 22:56:42 +0100

    Clean up controllers to use our own getPDO method
    Uses the getPDO function within the Database class in rapid, and also
    removes one unwanted controller and view called Example and example.php

--- controllers/UpdateShoe.php
+++ controllers/UpdateShoe.php
@@ -1,19 +1,18 @@
 <?php require_once('./models/Shoe.php'); ?>
 <?php return function($req, $res) {
     
-    $db = new PDO('mysql:host=localhost;dbname=shoe_shop;
-charset=utf8mb4', 'root', '');
+$db = \Rapid\Database::getPDO();
 
-    $id = $req->query('update');
+$id = $req->query('update');
 
-    $arr = Shoe::findOneById($id, $db);
+$arr = Shoe::findOneById($id, $db);
 
-    $res->render('main', 'update-shoe', [
-        'message'   => $req->query('success')? 'Successful!': '',
-        'name'      => $arr[0]['name'],
-        'brand'      => $arr[0]['brand'],
-        'size'      => $arr[0]['size'],
-        'price'      => $arr[0]['price'],
-    ]);
+$res->render('main', 'update-shoe', [
+   'message'   => $req->query('success')? 'Successful!': '',
+   'name'      => $arr[0]['name'],
+   'brand'      => $arr[0]['brand'],
+   'size'      => $arr[0]['size'],
+   'price'      => $arr[0]['price'],
+]);
 
 } ?>
--- controllers/UpdateShoeProcess.php
+++ controllers/UpdateShoeProcess.php
@@ -1,18 +1,17 @@
 <?php return function($req, $res) { require_once('./models/Shoe.php'); 
 
-$db = new PDO('mysql:host=localhost;dbname=shoe_shop;
-charset=utf8mb4', 'root', '');
+    $db = \Rapid\Database::getPDO();
 
-$id= $_POST['shoe_id'];
+    $id= $_POST['shoe_id'];
 
-$shoe = new Shoe([
-    'name' => $req->body('name'),
-    'brand' => $req->body('brand'),
-    'size' => $req->body('size'),
-    'price' => $req->body('price')
-]);
+    $shoe = new Shoe([
+        'name' => $req->body('name'),
+        'brand' => $req->body('brand'),
+        'size' => $req->body('size'),
+        'price' => $req->body('price')
+    ]);
 
-$shoe->update($id, $db);
+    $shoe->update($id, $db);
 
-$res->redirect("/display-shoes");
+    $res->redirect("/display-shoes");
 }?>
