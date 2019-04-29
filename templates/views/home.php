3fdd01dca64b467695f5243c9c40f8ff54a08a63
Scott Christmas <D00215593@student.dkit.ie>
2019-04-29 14:16:34 +0100

    Change the layout of the home page
    This commit changes the layout of the home page depending on what type
    of user has logged in, or if there is no one that is logged in, the
    home page will ask the user to make an account or login.

--- templates/views/home.php
+++ templates/views/home.php
@@ -1,13 +1,99 @@
 <?php session_start(); ?>
-<h2>Home</h2>
+<!-- Done with help from: https://v4-alpha.getbootstrap.com/examples/ -->
 <?php 
     if($locals['role'] === 'user'){
-        echo "<p>Hello User, your username is: " . $locals['username'] . "</p>";
+        echo '<h5 class="text-center">Welcome back ' . $locals['username'] . ', to our website.</h5>
+        <div class="jumbotron">
+        <div class="container">
+          <h1 class="display-3">Welcome to the Big Brain Shoe shop</h1>
+          <p class="text-center">This website offers shoe products from all of the popular brands such as, Adidas, Nike and more.</p>
+          <p class="text-center"><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
+        </div>
+      </div>
+  
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4">
+            <h2>About Us</h2>
+            <p>We are a group of students that offer a web store that offers products that are up to date with the current trend and brands.</p>
+            <p><a class="btn btn-secondary" href="#" role="button">Welcome to the website&raquo;</a></p>
+          </div>
+          <div class="col-md-4">
+            <h2>Products we offer</h2>
+            <p>Our store is filled up with shoes from brands known around the world, please make sure to check out products out using the button below. </p>
+            <p><a class="btn btn-secondary" href="products" role="button">View products &raquo;</a></p>
+         </div>
+          <div class="col-md-4">
+            <h2>Reviews</h2>
+            <p>Our reviews page allows you to sumbit reviews on products that you have already used. This allows you to give insight to any buyer looking to see what others think of the product before they actually purchase it.</p>
+            <p><a class="btn btn-secondary" href="reviews" role="button">View reviews &raquo;</a></p>
+          </div>
+        </div>
+  
+        <hr>';
     }
     else if($locals['role'] === 'manager'){
-        echo "<p>Hello Manager, your username is: " . $locals['username'] . "</p>";
+        echo '<h5 class="text-center">Welcome Staff member: ' . $locals['username'] . '</h5>
+        <div class="jumbotron">
+        <div class="container">
+          <h1 class="display-3">Welcome to the Big Brain Shoe shop</h1>
+          <p class="text-center">This website offers shoe products from all of the popular brands such as, Adidas, Nike and more.</p>
+          <p class="text-center"><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
+        </div>
+      </div>
+  
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4">
+            <h2>About Us</h2>
+            <p>We are a group of students that offer a web store that offers products that are up to date with the current trend and brands.</p>
+            <p><a class="btn btn-secondary" href="#" role="button">Welcome to the website&raquo;</a></p>
+          </div>
+          <div class="col-md-4">
+            <h2>Products we offer</h2>
+            <p>Our store is filled up with shoes from brands known around the world, please make sure to check out products out using the button below. </p>
+            <p><a class="btn btn-secondary" href="products" role="button">View products &raquo;</a></p>
+         </div>
+          <div class="col-md-4">
+            <h2>Reviews</h2>
+            <p>Our reviews page allows you to sumbit reviews on products that you have already used. This allows you to give insight to any buyer looking to see what others think of the product before they actually purchase it.</p>
+            <p><a class="btn btn-secondary" href="reviews" role="button">View reviews &raquo;</a></p>
+          </div>
+        </div>
+  
+        <hr>';
     }
     else if (!isset($locals['username']) || $locals['username'] == ''){
-        echo '<p>You are not logged in, please log in to view the database. <a href='.'login'.'>Login here</a></p>';
+        echo '<div class="jumbotron">
+        <div class="container">
+          <h1 class="display-3">Welcome to the Big Brain Shoe shop</h1>
+          <p class="text-center">This website offers shoe products from all of the popular brands such as, Adidas, Nike and more.</p>
+          <p class="text-center"><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
+        </div>
+      </div>
+  
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4">
+            <h2>About Us</h2>
+            <p>We are a group of students that offer a web store that offers products that are up to date with the current trend and brands.</p>
+            <p><a class="btn btn-secondary" href="#" role="button">Welcome to the website&raquo;</a></p>
+          </div>
+          <div class="col-md-4">
+            <h2>Products we offer</h2>
+            <p>Our store is filled up with shoes from brands known around the world, please make sure to check out products out using the button below. </p>
+            <p><a class="btn btn-secondary" href="products" role="button">View products &raquo;</a></p>
+         </div>
+          <div class="col-md-4">
+            <h2>Reviews</h2>
+            <p>Our reviews page allows you to sumbit reviews on products that you have already used. This allows you to give insight to any buyer looking to see what others think of the product before they actually purchase it.</p>
+            <p><a class="btn btn-secondary" href="reviews" role="button">View reviews &raquo;</a></p>
+          </div>
+        </div>
+  
+        <hr>
+
+        <h5 class="text-center">You are not logged in, please <a href='.'login'.'>login</a> to view the products we have to offer.</h5>
+        <h5 class="text-center">If you do not have an account set up, you can register <a href='.'register'.'>here</a>.</h5>';
     }   
 ?>
--- templates/views/register.php
+++ templates/views/register.php
@@ -12,7 +12,7 @@
     }
   ?>
 <?php if ($message) { ?>
-    <p class='<?= $message['class'] ?>'><?= $message['message'] ?></p>
+    <p class='<?= $message['class'] ?> text-center'><?= $message['message'] ?></p>
   <?php } ?>
    
 <div class="container">
@@ -21,9 +21,9 @@
     <label class="sr-only">Username</label>
     <input id="Username" type="text" name='username' class="form-control" placeholder="Username" autofocus>
     <label class="sr-only">Email</label>
-    <input id="Email" type="email" name='email' class="form-control" placeholder="Email" autofocus>
+    <input id="Email" type="email" name='email' class="form-control" placeholder="Email">
     <label class="sr-only">Password</label>
-    <input id="Password" type="password" name="password" class="form-control" placeholder="Password" required>
+    <input id="Password" type="password" name="password" class="form-control" placeholder="Password">
     <div class="checkbox">
     </div>
     <button class="btn btn-lg btn-primary btn-block" type="submit">Register</button>
